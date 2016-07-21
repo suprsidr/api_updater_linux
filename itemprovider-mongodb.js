729ed361d8a5052141f198f054e314483a95b2c6
suprsidr <suprsidr@gmail.com>
2016-07-21 13:39:06 -0500

    remove unused imports

--- itemprovider-mongodb.js
+++ itemprovider-mongodb.js
@@ -3,10 +3,7 @@
  */
 var C = require('./config.js');
 var Db = require('mongodb').Db;
-var Connection = require('mongodb').Connection;
 var Server = require('mongodb').Server;
-var BSON = require('mongodb').BSON;
-var ObjectID = require('mongodb').ObjectID;
 
 ItemProvider = function() {};
 
